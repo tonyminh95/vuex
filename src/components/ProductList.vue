0655ce70b6d9c6ae2e58c2ba39b748e3a0f2bdb0
tonyminh95 <minhbui12121995@gmail.com>
2020-02-10 17:39:28 +0900

    getters

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -2,27 +2,35 @@
     <div>
         <h1>Product List</h1>
 
-        <ul>
+        <img v-if="loading" src="https://i.imgur.com/JfPpwOA.gif">
+
+        <ul v-else>
             <li v-for="product in products" :key="product.id">{{ product.title }} - {{ product.price }}</li>
         </ul>
     </div>
 </template>
 
 <script>
-import shop from '@/api/shop'
 import store from '@/store/index'
 
 export default {
+  data () {
+    return {
+      loading: false
+    }
+  },
+
   computed: {
     products () {
-      return store.state.products
+      return store.getters.availableProducts
     }
   },
 
   created () {
-    shop.getProducts(products => {
-      store.commit('setProducts', products)
-    })
+    this.loading = true
+
+    store.dispatch('fetchProducts')
+      .then(() => { this.loading = false })
   }
 }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import shop from '@/api/shop'
 
 Vue.use(Vuex)
 
@@ -9,14 +10,26 @@ export default new Vuex.Store({
   },
 
   getters: {
-    productsCount () {
-
+    availableProducts (state) {
+      return state.products.filter(product => product.inventory > 0)
     }
   },
 
   actions: {
-    fetchProducts () {
+    fetchProducts ({commit}) {
+      return new Promise((resolve, reject) => {
+        shop.getProducts(products => {
+          commit('setProducts', products)
 
+          resolve()
+        })
+      })
+    },
+
+    addToCart ({commit}, product) {
+      if (product.inventory > 0) {
+        commit('pushProductToCart', product)
+      }
     }
   },
 
